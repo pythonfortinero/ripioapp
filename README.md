39a0d67be37a5f345a19395bc190eaaa1f4dba22
Blas Castro <castro.blas.martin@gmail.com>
2018-08-18 12:58:49 -0300

    commit inicial

---
+++ README.md
@@ -0,0 +1,9 @@
+RIPIO APP
+========
+
+
+si tenes problemas con el volumen de postgres
+crealo a mano con :
+`
+docker volume create pgdata
+`
---
+++ monedaapp/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+
+
+@login_required(login_url='/accounts/login/')
+def index(request):
+    return render(request, 'moneda/index.html')
